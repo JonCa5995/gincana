087a8ca0e1bdcb2e66744bff0094089707b05652
jcaceres <jonca5995@gmail.com>
2023-05-14 17:41:41 +0200

    Validación que comprueba que un elemento sea único entre los registros que no tengan baja lógica.

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -1,4 +1,6 @@
 <?php
 
 // override core en language system validation or define your own en language validation message
-return [];
+return [
+	'unique_null' => 'The {field} field must contain a unique value for undeleted records.'
+];
