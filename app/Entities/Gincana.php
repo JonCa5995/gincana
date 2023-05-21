bd160af0f86db04dde54cf9b695d4e9d765c60ea
jcaceres <jonca5995@gmail.com>
2023-05-21 18:10:01 +0200

    Corregida la entidad Gincana

--- app/Entities/Gincana.php
+++ app/Entities/Gincana.php
@@ -6,7 +6,25 @@ use CodeIgniter\Entity\Entity;
 
 class Gincana extends Entity
 {
-    protected $datamap = [];
-    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
-    protected $casts   = [];
+	protected $attributes = [
+		'id' => null,
+		'ds_gincana' => null,
+		'estado' => 'creado',
+		'fc_inicio' => null,
+		'fc_fin' => null,
+		'created_at' => null,
+		'updated_at' => null,
+		'deleted_at' => null,
+	];
+    protected $datamap = [
+		 'ds_gincana' => 'dsGincana',
+		 'fc_inicio' => 'fcInicio',
+		 'fc_fin' => 'fcFin',
+	 ];
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at', 'fc_inicio', 'fc_fin'];
+    protected $casts   = [
+		 'id' => 'integer',
+		 'dsGincana' => 'string',
+		 'estado' => 'string',
+	 ];
 }
